45545b10e21f2cc3d8299fb3e46851e2673252b5
bkhonkhammy <Test@Test.com>
2024-08-28 13:22:27 +1000

    Basic version

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#Projet creation
+import pandas as pd
 
+df = pd.read_csv('morse.csv', names=['CHAR', 'MORSE'], header=None)
+to_convert = input('Enter a word : ').upper()
+converted = ""
 
+for letter in to_convert:
+    converted += df.MORSE[df.CHAR == letter].values[0]
+    converted += " "
 
-
-
-
-
-
-
+print(converted)
