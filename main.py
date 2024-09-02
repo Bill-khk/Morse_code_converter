2f2ba7435c8d4ce46e1c43deb78e15d4e4bec831
Bill Khk <bill.khk@gmail.com>
2024-09-02 15:25:42 +1000

    Add templates folder to staging area

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-import wave
+import signal
 import morse
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, Response, request
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import mpld3
 
 SECRET_KEY = "KEY"
 
-# ----------------- RETRIEVE WORD FROM USER -----------------
-to_convert = "GRACE"  # input('Enter a word : ').upper()
-converted = morse.word_to_morse(to_convert)
-print(converted)
-
 # -----------------MAIN------------------------
-# WAVEDATA = wave.Wave()
-# WAVEDATA.morse_to_wave(converted)
-# WAVEDATA.generate_sound()
-# WAVEDATA.show_sound()
+WAVEDATA = signal.Signal() # Global WAVEDATA to store sound
 
 # -----------------WEB APP -------------------
 app = Flask(__name__)
@@ -33,12 +26,17 @@ class Myform(FlaskForm):
 def home():
     form = Myform()
     if form.validate_on_submit():
+        global WAVEDATA
         converted_text = morse.word_to_morse(form.Text.data)
-        WAVEDATA = wave.Wave()
-        WAVEDATA.morse_to_wave(converted)
-        WAVEDATA.show_sound()
-        return render_template('home.html', form=form, result=converted_text)
+        WAVEDATA.morse_to_signal(converted_text)
+        return render_template('home.html', form=form, result=converted_text, WAVEDATA=WAVEDATA)
     return render_template('home.html', form=form)
+
+@app.route('/generate_audio', methods=['GET', 'POST'])
+def generate_audio():
+    global WAVEDATA
+    sound = WAVEDATA.generate_buffer_sound()
+    return Response(sound)
 
 if __name__ == "__main__":
     app.run()
--- signal.py
+++ signal.py
@@ -3,13 +3,13 @@ import pyaudio
 import matplotlib.pyplot as plt
 import matplotlib.figure
 import mpld3
-from io import StringIO
+from io import StringIO, BytesIO
 import wave
 
 import numpy as np
 from PIL import Image
 
-# WAVE PARAMETERS
+# signal PARAMETERS
 DEFAULT_BIT_RATE = 16000
 DEFAULT_FREQUENCY = 800
 UNIT = 0.1  # BASE UNIT FOR SIGNAL SPEED
@@ -17,59 +17,52 @@ LENGTH_DOT = 1 * UNIT
 LENGTH_DASH = 3 * UNIT
 
 
-class Wave:
+class Signal:
 
     def __init__(self, bit_rate=DEFAULT_BIT_RATE, freq=DEFAULT_FREQUENCY):
         self.Value = ''
         self.Bit_rate = bit_rate  # number of frames per second
-        self.Frequency = freq  # Hz, waves per second
+        self.Frequency = freq  # Hz, signals per second
 
     # CONVERT A MORSE CODE INTO CHAR SIGNAL
-    def morse_to_wave(self, word):
+    def morse_to_signal(self, word):
+        self.Value = ""  # Flushing the value since using global WAVEDATA
         for letter in word:
             if letter == '.':
-                self.Value += wave_dot()
+                self.Value += signal_dot()
             elif letter == '-':
-                self.Value += wave_dash()
+                self.Value += signal_dash()
             elif letter == ' ':
                 for x in range(int(max(self.Bit_rate, self.Frequency + 100) * LENGTH_DOT)):
                     self.Value += chr(0)
 
     # CONVERT CHAR SIGNAL TO SOUND VIA PyAudio
-    def generate_sound(self):
+    def generate_buffer_sound(self):
 
-        # Instantiate PyAudio and initialize PortAudio system resources (1)
-        PyAudio = pyaudio.PyAudio
-        p = PyAudio()
+        # Create an in-memory bytes buffer
+        audio_data = bytes(self.Value, 'latin1')
+        audio_buffer = BytesIO()
+        # put the data in the buffer
+        with wave.open(audio_buffer, 'wb') as wf:
+            wf.setnchannels(1)  # Mono
+            wf.setsampwidth(1)  # 1 byte per sample (8-bit PCM)
+            wf.setframerate(self.Bit_rate)
+            wf.writeframes(audio_data)
 
-        # Open stream (2)
+            # Move to the beginning of the BytesIO buffer
+            audio_buffer.seek(0)
 
-        InMemory_Wav = StringIO(self.Value)  # Create an in-memory WAV file
-        with wave.open(InMemory_Wav, 'rb') as wf:
-            stream = p.open(format=wf.get_format_from_width(),
-                        channels=1,
-                        rate=self.Bit_rate,
-                        output=True)
-
-        #InMemory_Wav.seek(0)
-        stream.read(0)
-        stream.stop_stream()
-        stream.close()
-        wf.terminate()
-
-        #audio_buffer.seek(0)  # Reset buffer position to the beginning
-        #return audio_buffer
-
+        return audio_buffer
 
     # SHOW THE VALUE OF THE SIGNAL ON PLOT
     def show_sound(self):
-        # Convert WAVEDATA to numerical values
-        wave_values = [ord(char) for char in self.Value]
+        # Convert signalDATA to numerical values
+        signal_values = [ord(char) for char in self.Value]
 
-        # Plot the waveform
+        # Plot the signalform
         plt.figure(figsize=(18, 4))
-        fig = plt.plot(wave_values, color='blue')
-        plt.title("Waveform Visualization of WAVEDATA")
+        fig = plt.plot(signal_values, color='blue')
+        plt.title("signalform Visualization of signalDATA")
         plt.xlabel("Sample Index")
         plt.ylabel("Amplitude (0 to 255)")
 
@@ -80,47 +73,49 @@ class Wave:
 
 
 # CREATE A SIGNAL
-def generate_wave(freq, bit_rate, sample_length, fill=False):
-    WAVE = ''
+def generate_signal(freq, bit_rate, sample_length, fill=False):
+    signal = ''
     bit_rate = max(bit_rate, freq + 100)
     nb_frames = int(bit_rate * sample_length)
-    # Generate wave
+    # Generate signal
     for x in range(nb_frames):
-        WAVE += chr(int(math.sin(x / ((bit_rate / freq) / math.pi)) * 127 + 128))
+        signal += chr(int(math.sin(x / ((bit_rate / freq) / math.pi)) * 127 + 128))
 
     if fill:
         rest_frames = nb_frames % bit_rate
         for x in range(rest_frames):
-            WAVE += chr(0)
+            signal += chr(0)
 
     for x in range(int(bit_rate * LENGTH_DOT)):
-        WAVE += chr(0)
-    return WAVE
+        signal += chr(0)
+    return signal
 
 
-def wave_dot():
-    return generate_wave(DEFAULT_FREQUENCY, DEFAULT_BIT_RATE, LENGTH_DOT)
+def signal_dot():
+    return generate_signal(DEFAULT_FREQUENCY, DEFAULT_BIT_RATE, LENGTH_DOT)
 
 
-def wave_dash():
-    return generate_wave(DEFAULT_FREQUENCY, DEFAULT_BIT_RATE, LENGTH_DASH)
+def signal_dash():
+    return generate_signal(DEFAULT_FREQUENCY, DEFAULT_BIT_RATE, LENGTH_DASH)
 
-def generate_sound(value, bit_rate):
-    print(bit_rate)
-    bit_rate = bit_rate  # Sample rate in Hz
 
-    # Instantiate PyAudio and initialize PortAudio system resources (1)
-    PyAudio = pyaudio.PyAudio
-    p = PyAudio()
+def generate_sound(buffer):
+    with wave.open(buffer, 'rb') as wf:
+        # Open PyAudio stream
+        # Instantiate PyAudio and initialize PortAudio system resources (1)
+        PyAudio = pyaudio.PyAudio
+        p = PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
 
-    # Open stream (2)
-    # Create an in-memory WAV file
-    audio_buffer = io.BytesIO()
-    with p.open(format=p.get_format_from_width(1),
-                channels=1,
-                rate=bit_rate,
-                output=True) as wf:
-        wf.writeframes(value)  # use .tobytes() ?
+        # Read all data at once #Option could be to use chunk
+        data = wf.readframes(wf.getnframes())
+        # Play the audio by writing all data to the stream
+        stream.write(data)
 
-    audio_buffer.seek(0)  # Reset buffer position to the beginning
-    return audio_buffer.read()
+        # Stop the PyAudio stream
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
